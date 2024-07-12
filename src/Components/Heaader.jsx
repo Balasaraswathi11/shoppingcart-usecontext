becafe8efad78d7d3ddaa9579887a12f09684206
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-07-12 17:41:32 +0530

    showmore button

--- src/Components/Heaader.jsx
+++ src/Components/Heaader.jsx
@@ -9,7 +9,7 @@ const Heaader = () => {
 
 <nav className="navbar navbar-expand-lg   navbar ">
   <div className="container">
-    <a className="navbar-brand " href="#">Electronics</a>
+    <a className="navbar-brand " href="#">Shopsy</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,14 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import "./Product.css";
 import Mycontext from './Mycontext';
 
 const Product = ({ product }) => {
   const { cart, setCart } = useContext(Mycontext);
+  const [showMore, setShowMore] = useState(false);
+  const limit = 100; 
 
+
+  //showmore toggle function
+  const handleToggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+
+  //add to cart function
   const addcart = () => {
     setCart([...cart, product]);
   };
-
+ //removecartfunction
   const removecart = () => {
     setCart(cart.filter((c) => c.id !== product.id));
   };
@@ -20,11 +29,18 @@ const Product = ({ product }) => {
       <img src={product.image} className="card-img-top img" alt="product" />
       <div className="card-body text-center">
         <h5 className="card-title fs-5">{product.name}</h5>
-        <p className="card-text">{product.description}</p>
+        <p className="card-text">
+          {showMore ? product.description : `${product.description.slice(0, limit)}${product.description.length > limit ? '...' : ''}`}
+          {product.description.length > limit && (
+            <a onClick={handleToggleShowMore} className="show-more">
+              {showMore ? 'Show Less' : 'Show More'}
+            </a>
+          )}
+        </p>
         <p className="card-text">
           <b>Rating:</b> {product.rating.rate} ({product.rating.count}) <i className="fa fa-star text-warning"></i>
         </p>
-        <p className="card-text"> Price: <b>{product.price}</b></p>
+        <p className="card-text"> Price: $ <b>{product.price}</b></p>
         {isInCart ? (
           <button className='btnremove' onClick={removecart}>Remove from cart</button>
         ) : (
